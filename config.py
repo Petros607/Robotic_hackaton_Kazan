e01ffec4967eb9bef69f6c4d45b2f61874fb81ec
Leonov Egor <crozetle@gmail.com>
2024-08-11 13:59:25 +0400

    1. Добавил файл config.py для задания значений окружения 2. Провел инициализацию алембика 3. Провел первую миграцию 4. Создал файл enums.py содержащий все enum (что в models.py лежали только models) 5. Добавил metadata в models.py для последующего использования в миграциях 6. Изменил определение моделей на более современное (через наследование класса)

---
+++ config.py
@@ -0,0 +1,12 @@
+# данный скрипт вытягивает все чувствительные данные из .env и подставляет туда, где это необходимо
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
