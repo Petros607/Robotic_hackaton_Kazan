95cb60f6cb33d061f2784d4b53d9324ef96b476a
Leonov Egor <crozetle@gmail.com>
2024-08-11 20:06:20 +0400

    Переместил config.py в корень. Так как данный скрипт напрямую связан с параметрами окружения ВСЕГО проекта, а не только src

--- config.py
+++ config.py
@@ -10,3 +10,5 @@ DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
+
+DB_CONNECT_STRING = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
