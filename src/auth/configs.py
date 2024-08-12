527ea80cf4980455bcc5cbd6e5806c59f3ede26a
Sapdotten <semenova.a.p.04@gmail.com>
2024-08-12 19:36:03 +0300

    add jwt token

---
+++ src/auth/configs.py
@@ -0,0 +1,16 @@
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN_CONFIGS = "src/auth/token_configs.json"
+
+with open(TOKEN_CONFIGS, 'r') as f:
+    token_configs = json.load(f)
+    
+ALGORITHM = token_configs["ALGORITHM"]
+ACCESS_TOKEN_EXPIRE_MINUTES = token_configs["ACCESS_TOKEN_EXPIRE_MINUTES"]
+REFRESH_TOKEN_EXPIRE_DAYS = token_configs["REFRESH_TOKEN_EXPIRE_DAYS"]
+
+SECRET_KEY = os.environ.get("JWT_KEY")
